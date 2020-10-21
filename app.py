5552b2440ce1b8165d9c8d080d5b9fbbab05507e
kgyeongmi21 <kgyeongmi21@naver.com>
2020-10-22 01:22:59 +0900

    라우팅 및 템플릿 추가

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
+from flask import Flask, render_template
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -12,3 +12,27 @@ app.config['SECRET_KEY'] = 'Xylq5waRm51AeOky0pnSeopVNdSlKRXTrgcRzTBMm8gAGUG22miV
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return render_template("list.html")
+
+
+@app.route('/article/create')
+def post_create():
+    return render_template("create.html")
+
+
+@app.route('/article/<postid>')
+def post_detail():
+    return render_template("detail.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+if __name__ == '__main__':
+    app.run()
